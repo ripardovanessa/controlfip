c8f962ecb18397de5146e5fb4ed050d9b5336d30
vanessa ripardo <vanessa.ripardo38@gmail.com>
2019-05-03 21:35:43 -0300

    Arquivos principal do sistema

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>HOME | ControlFip</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>ControlFip</h1>
+		<h2>Sistema de Controle Financeiro Pessoal</h2>
+
+	</body>
+</html>
