16810a315cd43b2afc2c2b03d53cbb7a24e0e3fa
Vanessa Ripardo <vanessa.ripardo38@gmail.com>
2019-05-14 21:28:58 -0300

    Atualização do index

--- index.php
+++ index.php
@@ -11,5 +11,25 @@
 			<a href="contas-apagar.php">Contas a pagar</a>
 			<a href="contas-areceber.php">Contas a receber</a>
 		</div>
+		<div>
+			<table>
+				<thead>
+					<tr>
+						<th>Mês</th>
+						<th>Total previsto</th>
+						<th>Total real</th>
+						<th>Saldo em Carteira</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Abril</td>
+						<td>R$ 5000.00</td>
+						<td>R$ 3000.00</td>
+						<td>R$ 4200.00</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	</body>
 </html>
